444a12b0c1c0820c6795d38d1b30ce70bd3ac1d0
Mai Anh Vu <maianhuel@gmail.com>
2024-06-24 22:21:20 +0700

    [e] edit path

--- src/main_seg_cls.py
+++ src/main_seg_cls.py
@@ -88,7 +88,7 @@ class Config:
 
     # k-fold
     k = 4
-    folds_file = f"{DATA_PATH}/input/folds_{k}.csv"
+    folds_file = os.path.join(DATA_PATH, "folds_{k}.csv")
     selected_folds = [0, 1, 2, 3]
 
     # Model
