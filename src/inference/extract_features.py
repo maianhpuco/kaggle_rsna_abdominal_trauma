ce81285d7297a28284b588126951a5e7e8a49525
Mai Anh Vu <maianhuel@gmail.com>
2024-06-26 15:39:37 +0700

    [e] retrain -> store true

--- src/inference/extract_features.py
+++ src/inference/extract_features.py
@@ -88,9 +88,9 @@ def predict_distributed(
             preds.append(y_pred.detach())
             fts.append(ft.detach())
             
-            # # Optional: Clear GPU memory periodically
-            # if len(preds) % 100 == 0:
-            #     torch.cuda.empty_cache()
+            # Optional: Clear GPU memory periodically
+            if len(preds) % 100 == 0:
+                torch.cuda.empty_cache()
 
     preds = torch.cat(preds, 0)
     fts = torch.cat(fts, 0)
